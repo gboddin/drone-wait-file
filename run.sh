35bc18d5c14395de373c21994b42bf5666e27ffa
Gregory Boddin <gregory@siwhine.net>
2018-02-26 18:40:26 +0100

    Cleaned it

--- run.sh
+++ run.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 function plugin_provide {
-    SOURCE_FILE="${PLUGIN_SOURCE}"
-    TARGET_FILE="${PLUGIN_TARGET}"
-    if [ ! -f "${SOURCE_FILE}" ]; then
-      touch "${TARGET_FILE}"
+    if [ ! -f "${PLUGIN_SOURCE}" ]; then
+      touch "${PLUGIN_TARGET}"
     else
-      mv "${SOURCE_FILE}" "${TARGET_FILE}"
+      mv "${PLUGIN_SOURCE}" "${PLUGIN_TARGET}"
     fi
 }
 
 function plugin_wait {
-    TARGET_FILE=${PLUGIN_TARGET}
     while /bin/true; do
         [ ! -f ${PLUGIN_TARGET} ] && sleep 1 && continue
         if [ $(stat -c%s ${PLUGIN_TARGET}) -lt 1 ]; then
